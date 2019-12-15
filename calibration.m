f53becf7a01bf4723e5a437b8cf5b261294767e0
Nicholas Giacoboni <58661831+GiacoboniNicholas@users.noreply.github.com>
2019-12-15 18:19:46 +0100

    Update calibration.m

--- calibration.m
+++ calibration.m
@@ -1,27 +1,29 @@
 %**************************************************************************
 %     Copyright (C) 2019  Nicholas Giacoboni
-% 
+%
 %     This program is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
 %     the Free Software Foundation, either version 3 of the License, or
 %     (at your option) any later version.
-% 
+%
 %     This program is distributed in the hope that it will be useful,
 %     but WITHOUT ANY WARRANTY; without even the implied warranty of
 %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %     GNU General Public License for more details.
-% 
+%
 %     You should have received a copy of the GNU General Public License
 %     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %**************************************************************************
 %% CALIBRATE YOUR LOADCELL WITH ARDUINO
-classdef calibration 
-    properties
-        n                  % Number of readings
-        known_weight       % Weight used for calibration
+classdef calibration < dynamicprops
+    properties(Access = public)
+        n = 100               % Number of readings
+        known_weight = 1      % Weight used for calibration
+    end
+    properties(Access = private)
         tare_weight = 0
         scale_factor = 1
-    end   
+    end
     methods(Access = public)
         %% CONSTRUCTOR
         function cal = calibration(varargin)
@@ -32,37 +34,29 @@ classdef calibration
                 error('Too many input arguments.');
             end
             % Check the inputs
-            if varargin{1} < 0
+            if varargin{1} < 0 
                 error('The number of readings must be positive');
-            end
-            if ~isnumeric(varargin{1})
-                error('Incorrect type');
-            end
-            if ~isscalar(varargin{1})
+            elseif ~isnumeric(varargin{1}) || ~isscalar(varargin{1})
                 error('Incorrect type');
             end
             if varargin{2} == 0
                 error('The known weight can not be zero');
-            end
-            if ~isnumeric(varargin{2})
-                error('Incorrect type');
-            end
-            if ~isscalar(varargin{2})
+            elseif ~isnumeric(varargin{2}) || ~isscalar(varargin{2})
                 error('Incorrect type');
             end
             cal.n = varargin{1};
             cal.known_weight = varargin{2};
         end
         %% Tare
-        function tare_value = tare(varargin)
+        function value = tare(varargin)
             if nargin < 2
                 error('MyComponent:incorrectType',...
                     'Not enough input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order.');
+                    'class objects in this order.');
             elseif nargin > 2
                 error('MyComponent:incorrectType',...
                     'Too many input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order.');
+                    'class objects in this order.');
             end
             obj = varargin{1};
             HX711_obj = varargin{2};
@@ -73,18 +67,19 @@ classdef calibration
                     value(i) = NaN;
                 end
             end
-            tare_value = mean(value,'omitnan');
+            value = mean(value,'omitnan');
+            obj.tare_weight = value;
         end
         %% Scale factor
-        function scale_value = scale(varargin)
+        function value = scale(varargin)
             if nargin < 2
                 error('MyComponent:incorrectType',...
                     'Not enough input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order.');
+                    'class objects in this order.');
             elseif nargin > 2
                 error('MyComponent:incorrectType',...
                     'Too many input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order.');
+                    'class objects in this order.');
             end
             obj = varargin{1};
             HX711_obj = varargin{2};
@@ -96,25 +91,32 @@ classdef calibration
                 end
             end
             ev_value = mean(value,'omitnan');
-            scale_value = (ev_value - obj.tare_weight)/obj.known_weight;
+            value = (ev_value - obj.tare_weight)/obj.known_weight;
+            obj.scale_factor = value;
         end
         %% Get weight
         function weight = get_weight(obj,HX711_obj,varargin)
             if nargin < 2
                 error('MyComponent:incorrectType',...
                     'Not enough input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order.');
+                    'class objects in this order.');
             elseif nargin > 3
                 error('MyComponent:incorrectType',...
                     'Too many input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order. You can also provide the number of readings');
+                    'class objects in this order. You can also provide the number of readings');
             end
-            if isempty(varargin)==1
+            if ~isempty(varargin)
+                if ~isnumeric(varargin{1}) || ~isscalar(varargin{1})
+                    error('Incorrect type');
+                end
+            end
+            if isempty(varargin)
                 % Raw reading
                 val = read_HX711(HX711_obj);
-                weight = (val - obj.tare_weight)/obj.scale_factor;
                 if check_conversion(obj,val)
                     weight = NaN;
+                else
+                    weight = (val - obj.tare_weight)/obj.scale_factor;
                 end
             else
                 % Average of multiple readings
@@ -135,11 +137,14 @@ classdef calibration
             if nargin < 3
                 error('MyComponent:incorrectType',...
                     'Not enough input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order then the number of readings.');
+                    'class objects in this order then the number of readings.');
             elseif nargin > 3
                 error('MyComponent:incorrectType',...
                     'Too many input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order then the number of readings');
+                    'class objects in this order then the number of readings');
+            end
+            if ~isnumeric(k) || ~isscalar(k)
+                error('Incorrect type');
             end
             weight = 1:1:k;
             for i=1:1:k
@@ -157,11 +162,16 @@ classdef calibration
             if nargin < 3
                 error('MyComponent:incorrectType',...
                     'Not enough input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order then the number of readings and the weight (if it is known).');
+                    'class objects in this order then the number of readings and the weight (if it is known).');
             elseif nargin > 5
                 error('MyComponent:incorrectType',...
                     'Too many input arguments:\nprovide *calibration* and *HX711* %s',...
-                        'class objects in this order then the number of readings and the weight (if it is known).');
+                    'class objects in this order then the number of readings and the weight (if it is known).');
+            end
+            if ~isempty(varargin)
+                if ~isnumeric(varargin{1}) || ~isscalar(varargin{1})
+                    error('Incorrect type');
+                end
             end
             [average,std_dev] = stat(obj,HX711_obj,k);
             x = linspace(average-4*std_dev,average+4*std_dev,5000);
@@ -187,10 +197,10 @@ classdef calibration
         %% Check HX711 data
         function incorrect = check_conversion(~,val)
             if (val == hex2dec('800000') || val == hex2dec('7FFFFF'))
-                incorrect = true;
+                incorrect = true; 
             else
                 incorrect = false;
             end
-        end 
+        end
     end
 end
